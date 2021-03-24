a238b3b780637ae27bfa10bd9abff532d7371e66
aysingh2 <ayushsingh1412@outlook.com>
2021-03-24 23:06:20 +0530

    new commit

--- 20210316_Strings.py
+++ 20210316_Strings.py
@@ -15,4 +15,35 @@ print(b[0:3])
 print(b.split(' '))
 print(b.upper().replace(b.upper() ,"testing"))
 
+age = 36
+txt = "My name is John, I am " + str(age)
+print(txt)
+
+
+age = 36
+txt = "My name is John, I am {}"
+print(txt.format(age))
+
+quantity = 3
+itemno = 567
+price = 49.95
+myorder = "I want {0} pieces of item {2} for {1} dollars."
+print(myorder.format(quantity, itemno, price))
+
+
+
+text = 'hi this\'s is ayush'
+print(text)
+
+
+txt = "Hello, welcome to my world."
+
+x = txt.endswith("my world.")
+
+print(x)
+
+
+
+
+
 #print('This' not in b)
