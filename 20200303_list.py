1e0c614b52ee0524f6370a676904140bbe3071f0
aysingh2 <ayushsingh1412@outlook.com>
2021-04-09 23:01:10 +0530

    new commit

--- 20200303_list.py
+++ 20200303_list.py
@@ -13,3 +13,52 @@ print(b)
 a[0]='b'
 
 print(a[-1])
+x=[]
+
+x.append(('a','f','g'))
+print(x)
+
+
+
+thislist = ["apple", "banana", "cherry"]
+tropical = ["mango", "pineapple", "papaya"]
+thislist.extend(tropical)
+print(thislist)
+
+
+thislist = ["apple", "banana", "cherry"]
+thistuple = ("kiwi", "orange")
+thislist.extend(thistuple)
+print(thislist)
+
+
+
+#e,f = thislist[1:2]
+e= thislist
+
+e.insert(0, "the")
+
+
+e.append("asasda")
+
+
+print(e)
+
+
+e.remove("the")
+
+e.pop()
+
+del e[1]
+
+e.clear()
+print(e)
+
+
+print(thislist)
+
+j = ['amit','ankit','sanjay']
+
+for k in j:
+    print(k)
+
---
+++ 20210406_clearList.py
@@ -0,0 +1,9 @@
+a = [1,2,4,5,7]
+b=a
+
+#a.clear()
+
+a.clear()
+
+print(a)
+print(b)
